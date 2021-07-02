03f35cf085075a75057163e151ec6d89f01870dc
Long <37376007+long1710@users.noreply.github.com>
2021-07-01 22:24:31 -0500

    Update mongodb connection, uri and example code for mongodb

--- BackEnd/microservices/auth-server/src/main/java/com/example/authserver/controller/ExampleController.java
+++ BackEnd/microservices/auth-server/src/main/java/com/example/authserver/controller/ExampleController.java
@@ -1,5 +1,8 @@
 package com.example.authserver.controller;
 
+import com.example.authserver.domain.Test;
+import com.example.authserver.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class ExampleController {
 
+    @Autowired
+    private TestRepository repository;
+
     @GetMapping("/test")
     public ResponseEntity test1(){
         return ResponseEntity.ok("test from service 1");
+    }
+
+    @GetMapping("/mongoTest")
+    public ResponseEntity test2() {
+
+        repository.save(Test.builder().firstName("Alice").lastName("Smith").build());
+        repository.save(Test.builder().firstName("Bob").lastName("Smith").build());
+
+        return ResponseEntity.ok("test from service 2");
     }
 }
